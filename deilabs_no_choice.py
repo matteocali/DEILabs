c38dde48fbd123af7608d291d2711a3c47032fb9
Matteo Caligiuri <74826391+caligola25@users.noreply.github.com>
2023-03-01 10:59:56 +0100

    DEILabs

---
+++ deilabs_no_choice.py
@@ -0,0 +1,67 @@
+import sys
+import getopt
+from bs4 import BeautifulSoup
+from pytimedinput import timedInput
+import requests
+
+
+def arg_parser(argv):
+    """
+    Function used to parse the input argument given through the command line
+        param:
+            - argv: system arguments
+        return: 
+            - list containing the input and output path
+    """
+
+    # Argument containing the name of the lab to log in
+    arg_lab_name = "lttm"
+    # Help string
+    arg_help = "{0} -l <lab>".format(argv[0])
+
+    try:
+        # Recover the passed options and arguments from the command line (if any)
+        opts, args = getopt.getopt(
+            argv[1:], "hl:", ["help", "lab="])
+    except getopt.GetoptError:
+        print(arg_help)  # If the user provide a wrong options print the help string
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-l", "--lab"):
+            arg_lab_name = arg  # Set the name of the lab
+
+    return [arg_lab_name]
+
+
+if __name__ == '__main__':
+    login = {
+        "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d": "eyJpdiI6IlwvNTYrWkdqenNVallmY2QwTkpMNk53PT0iLCJ2YWx1ZSI6IjdoUkFqQlBSZGZUYXJycmtNbEpYUXU0b0lndHBOQVRGQ3p4aWhyNVNBNHc9IiwibWFjIjoiNTAwY2JmYzQ4M2RiM2U1OTQ0YzkzNzBlNTRkMzIwZWQwYTUzYjJmODBhNTcwZGJkZWI3NWVjOTExNmRlMjcwYyJ9"
+    }
+    url = "https://deilabs.dei.unipd.it/laboratory_in_outs"
+    
+    lab_id = {"lttm": 20, 
+              "signet": 47, 
+              "mian": 8}
+
+    lab_name = arg_parser(sys.argv)[0]
+    if lab_name not in lab_id.keys():
+        print("Impossible to login, invalid lab name")
+        sys.exit(2)
+
+    with requests.Session() as s:
+        response = s.get(url, cookies=login)
+        dom = BeautifulSoup(response.text, 'html.parser')
+        form = dom.find("form", {"id": "edit_laboratory_in_outs_form"})
+        # check if logged in already
+        if form is not None:
+            print("User already logged in")
+        else:
+            # we need to login
+            form = dom.find("form", {"id": "create_laboratory_in_out_form"})
+            data = {c.attrs["name"]: c.attrs["value"] for c in form.findChildren("input", recursive=False)}
+            data["laboratory_id"] = lab_id[lab_name]
+            io = s.post(url=form.attrs["action"], json=data)
+            dom = BeautifulSoup(io.text, 'html.parser')
+            res = dom.find("div", {"class": "alert"}).findChildren("span", recursive=False)
+            print(f"Succesfully loggedein in {lab_name}:", res[0].text)
---
+++ launch.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+### USER DEFINED VAR
+LABNAME=signet
+
+CONDAPATH=~/miniconda3
+ENVNAME=dei_labs
+
+FIRSTHOUR=09
+MINUTES=00
+
+LINUXDESKTOP=gnome  # gnome, kde
+
+SCRIPTPATH=
+
+
+### PREDEFINED VAR (do not modify)
+BASEENV=base
+
+# Set env path
+if [ $ENVNAME = base ]; then
+	ENVPATH=$CONDAPATH
+else
+	ENVPATH=$CONDAPATH/envs/$ENVNAME
+fi
+
+PYTHONPATH=$ENVPATH/bin
+
+# Set Linux Desktop
+if [ $LINUXDESKTOP = gnome ]; then
+	LOCK_STATUS=$(qdbus org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.GetActive)  # For GNOME
+else
+	LOCK_STATUS=$(qdbus org.freedesktop.ScreenSaver /ScreenSaver GetActive)  # For KDE
+fi
+
+# Create next file if not already present
+FILE=$SCRIPTPATH/data/next
+if [ ! -e "$FILE" ] ; then
+    echo 0 > $FILE
+fi
+
+# Reade the list time of execution of at
+while read line; do
+    ATQ=$line
+    break
+done < $FILE
+
+RANDOM_MIN=$(shuf -i 30-60 -n 1)  # Define a random number of minutes between 30 and 60
+
+# Execute the login script and reschedule
+if [ $LOCK_STATUS = false ]; then
+	OUT=$($PYTHONPATH/python3 $SCRIPTPATH/deilabs_no_choice.py -l $LABNAME)
+	notify-send -u critical -i /usr/share/icons/gnome/scalable/places/poi-building.svg DEILabs "$OUT"  # Notify the user
+	
+	RES=$(( $ATQ <= $(date +%s) ))
+	if [ $RES = 1 ]; then
+		at $FIRSTHOUR:$MINUTES tomorrow -f $SCRIPTPATH/launch.sh  # Add a job for tomorrow only if not already present
+		echo $(date +%s -d "$FIRSTHOUR:$MINUTES tomorrow") > $FILE
+	fi
+else
+	echo $(date +%s -d "now + $RANDOM_MIN min") > $FILE
+	at now + $RANDOM_MIN min -f $SCRIPTPATH/launch.sh
+fi
